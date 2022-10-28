5d691d6a7a71e40c92c8bbe82a049d05b8d581ec
GArifullinII <ildaridrisovich@gmail.com>
2022-10-28 18:42:39 +0400

    feat: add func parseJSON

--- WeatherYA/Model/NetworkWeatherManager.swift
+++ WeatherYA/Model/NetworkWeatherManager.swift
@@ -34,4 +34,22 @@ struct NetworkWeatherManager {
         
         task.resume()
     }
+    
+    // MARK: - Parse JSON
+    func parseJSON(withData data: Data) -> Weather? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let weatherData = try decoder.decode(WeatherData.self, from: data)
+            guard let weather = Weather(weatherData: weatherData) else {
+                return nil
+            }
+            
+            return weather
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        
+        return nil
+    }
 }
