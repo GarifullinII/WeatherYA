f89da30f04e4704d47cfc7842bed10e7f39afa73
GArifullinII <ildaridrisovich@gmail.com>
2022-10-31 23:05:25 +0400

    feat: install pod and SwiftSVG

--- WeatherYA/View/DetailViewController.swift
+++ WeatherYA/View/DetailViewController.swift
@@ -9,10 +9,42 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var nameCityLabel: UILabel!
     
+    @IBOutlet var viewCity: UIView!
+    
+    @IBOutlet var conditionLabel: UILabel!
+    
+    @IBOutlet var temperaturaCity: UILabel!
+    
+    @IBOutlet var pressureLabel: UILabel!
+    
+    @IBOutlet var windSpeedLabel: UILabel!
+    
+    @IBOutlet var minTempLabel: UILabel!
+    
+    @IBOutlet var maxTempLabel: UILabel!
+    
+    var weatherModel: Weather?
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    // MARK: - Refresh labels
+    
+    func refreshLabels() {
+        
+        self.nameCityLabel.text = weatherModel?.name
+        
+        self.conditionLabel.text = weatherModel?.conditionCode
+        
+        self.pressureLabel.text = "\((weatherModel?.pressureMm)!)"
+        
+        self.windSpeedLabel.text = "\((weatherModel?.windSpeed)!)"
+        
+        self.minTempLabel.text = "\((weatherModel?.tempMin)!)"
+        
+        self.maxTempLabel.text = "\((weatherModel?.tempMax)!)"
+    }
 }
